9412f311b12d15c55d59d8c7903c9ffea4f966d3
cristianacenteio <cpcenteio@gmail.com>
2024-11-15 23:53:34 +0000

    Created and implemented "Delete event" and "Add event"

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin, { Draggable, DropArg} from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Dialog, DialogPanel, Transition } from "@headlessui/react";
+import { CheckIcon, ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 interface Event {
   title: string;
@@ -64,6 +66,42 @@ export default function Home() {
     setIdToDelete(Number(data.event.id))
   }
 
+  function handleDelete() {
+    setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)))
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+
+  function handleCloseModal() {
+    setShowModal(false)
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    })
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewEvent({
+      ...newEvent,
+      title: e.target.value
+    })
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setAllEvents([...allEvents, newEvent])
+    setShowModal(false)
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    })
+  }
 
   return (
     <>
@@ -82,9 +120,9 @@ export default function Home() {
             headerToolbar={{
               left: 'prev,next today',
               center: 'title',
-              right: 'resourceTimelineWook, dayGridMonth, timeGridWeek'
+              right: 'dayGridMonth, timeGridWeek'
             }}
-            events={{allEvents}}
+            events={allEvents}
             nowIndicator={true}
             editable={true}
             droppable={true}
@@ -107,6 +145,130 @@ export default function Home() {
               </div>
             ))}
         </div>
+            <Transition.Root show={showDeleteModal} as={Fragment}>
+              <Dialog as="div" className="relative z-10" onClose={setShowDeleteModal}>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      >
+                      <DialogPanel className="relative transform overflow-hidden rounded-lg
+                        bg-white text-left shadow-xl transistion-all sm:my-8 sm:max-w-lg">
+                          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                            <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center
+                            justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                <ExclamationTriangleIcon className="h-6 w-6 text-red-600 aria-hidden:'true'" />
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                DeleteEvent
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p>
+                                  Are you sure you want to delete this event?
+                                </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="bg-gray-50 px-4 py-3 sm:flex-row-reverse sm:px-6">
+                            <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 pt-2 text-sm
+                            semibold text-white shadow-sm hover:bg-red-900 sm:ml-3 sm:w-auto" onClick={handleDelete}>
+                              Delete
+                            </button>
+                            <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 pt-2 text-sm
+                            ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:ml-3 sm:w-auto" onClick={handleCloseModal}>
+                              Cancel
+                            </button>
+                          </div>
+
+                      </DialogPanel>
+                      </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition.Root>
+
+            <Transition.Root show={showModal} as={Fragment}>
+              <Dialog as="div" className="relative z-10" onClose={setShowModal}>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      >
+                      <DialogPanel className="relative transform overflow-hidden rounded-lg
+                        bg-white text-left shadow-xl transistion-all sm:my-8 sm:max-w-lg">
+                          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                            <div className="sm:flex sm:items-start">
+                              <CheckIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                Add Event
+                              </Dialog.Title>
+                              <form action="submit" onSubmit={handleSubmit}>
+                              <div className="mt-2">
+                                <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900
+                                shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                                focus:ring-violet-600 sm:text-sm sm:leading-6" value={newEvent.title} onChange={(e) => handleChange(e)}
+                                placeholder="Title" />
+                              </div>
+                          <div className="mt-5 sm:mt-6 sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                            <button type="submit" className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 pt-2 text-sm
+                            semibold text-white shadow-sm hover:bg-red-900 sm:ml-3 sm:w-auto" disabled={newEvent.title === ''}>
+                              Create
+                            </button>
+                            <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 pt-2 text-sm
+                            ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:ml-3 sm:w-auto" onClick={handleCloseModal}>
+                              Cancel
+                            </button>
+                          </div>
+                          </form>
+                        </div>
+                        </div>
+                      </DialogPanel>
+                      </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition.Root>
        </div>
       </main>
     </>
